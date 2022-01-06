2445f69487430596bbfbf3d907f7b63fdd1a273f
kimuksung <kimuksung2@daum.net>
2022-01-06 23:12:28 +0900

    프로그래머스

--- 약수의 개수와 덧셈.py
+++ 약수의 개수와 덧셈.py
@@ -1,12 +1,11 @@
 def solution ( left , right ) :
     answer = 0
     temp = [1]*1001
-    for i in range ( 31 ) :
+    for i in range ( 32 ) :
         temp[i*i] = -1
 
     for data in range ( left, right + 1) :
         answer +=  data * temp[data] 
-    #print (temp)
     return answer
 
 print ( solution ( 1 , 4) )
