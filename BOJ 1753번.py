d2cb017d18311033b737c640c6db8036e7e310fe
kimuksung <kimuksung2@daum.net>
2022-08-30 23:58:22 +0900

    백준 - 다익스트라

---
+++ BOJ 1753번.py
@@ -0,0 +1,34 @@
+#BOJ 1753번
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+#init
+V,E = map(int,input().split())
+start = int(input())
+graphs = [[] for _ in range(V+1)]
+table = [float('INF')]*(V+1)
+table[start]=0
+q = []
+heapq.heappush(q,(0,start))
+
+#Graph
+for _ in range(E):
+    st,en,value = map(int,input().split())
+    graphs[st].append((en,value))
+
+while q:
+    value,node = heapq.heappop(q)
+    #print(node,value,q)
+    # 1. Check
+    if table[node]!=value :
+        continue
+
+    # 2. table 갱신 및 heapq 추가
+    for end,value in graphs[node]:
+        if table[end] > table[node]+value :
+            table[end] = table[node]+value
+            heapq.heappush(q,(table[node]+value,end))
+
+[ print('INF') if data==float('INF') else print(data) for data in table[1:] ]
